5b81eddc1266d6774a261f77fd1f64aff5cbf9a5
yarden <yarden9292@gmail.com>
2021-08-16 02:40:03 +0300

    fix rrc filter

--- my_files/src/pulse_shaping.py
+++ my_files/src/pulse_shaping.py
@@ -1,5 +1,5 @@
 import numpy as np
-from commpy.filters import rrcosfilter
+# from lib.packages.CommPy.build.lib.commpy.filters import rrcosfilter
 
 from my_files.src import visualizer, params as p
 
@@ -15,3 +15,55 @@ def pulse_shaping(i_q_samples: np.ndarray) -> np.ndarray:
     qW = np.convolve(h_rrc, i_q_samples)  # Waveform with PSF
 
     return qW
+
+
+def rrcosfilter(N, alpha, Ts, Fs):
+    """
+    Generates a root raised cosine (RRC) filter (FIR) impulse response.
+
+    Parameters
+    ----------
+    N : int
+        Length of the filter in samples.
+
+    alpha : float
+        Roll off factor (Valid values are [0, 1]).
+
+    Ts : float
+        Symbol period in seconds.
+
+    Fs : float
+        Sampling Rate in Hz.
+
+    Returns
+    ---------
+
+    time_idx : 1-D ndarray of floats
+        Array containing the time indices, in seconds, for
+        the impulse response.
+
+    h_rrc : 1-D ndarray of floats
+        Impulse response of the root raised cosine filter.
+    """
+
+    T_delta = 1/float(Fs)
+    time_idx = ((np.arange(N)-N/2))*T_delta
+    sample_num = np.arange(N)
+    h_rrc = np.zeros(N, dtype=float)
+
+    for x in sample_num:
+        t = (x-N/2)*T_delta
+        if t == 0.0:
+            h_rrc[x] = 1.0 - alpha + (4*alpha/np.pi)
+        elif alpha != 0 and t == Ts/(4*alpha):
+            h_rrc[x] = (alpha/np.sqrt(2))*(((1+2/np.pi)* \
+                    (np.sin(np.pi/(4*alpha)))) + ((1-2/np.pi)*(np.cos(np.pi/(4*alpha)))))
+        elif alpha != 0 and t == -Ts/(4*alpha):
+            h_rrc[x] = (alpha/np.sqrt(2))*(((1+2/np.pi)* \
+                    (np.sin(np.pi/(4*alpha)))) + ((1-2/np.pi)*(np.cos(np.pi/(4*alpha)))))
+        else:
+            h_rrc[x] = (np.sin(np.pi*t*(1-alpha)/Ts) +  \
+                    4*alpha*(t/Ts)*np.cos(np.pi*t*(1+alpha)/Ts))/ \
+                    (np.pi*t*(1-(4*alpha*t/Ts)*(4*alpha*t/Ts))/Ts)
+
+    return time_idx, h_rrc
