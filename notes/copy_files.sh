43eb946df10f3396ee03c505430a552201985d34
yarden <yarden9292@gmail.com>
2022-09-04 14:08:03 +0300

    added copy_files.sh script

---
+++ notes/copy_files.sh
@@ -0,0 +1,28 @@
+echo """
+run the following commands (dont copy):
+  ssh yarcoh@132.72.49.187 du -hd1 ./projects/thesis-code4/data/datasets
+  scp -r yarcoh@132.72.49.187:projects/thesis-code4/data/... ~/Desktop/projects/thesis_stuff/Thesis-code/data/...
+"""
+echo "here are the datasets:"
+ssh yarcoh@132.72.49.187 du -hd1 ./projects/thesis-code4/data/datasets
+
+echo "\nand here are the models:"
+ssh yarcoh@132.72.49.187 du -hd1 ./projects/thesis-code4/data/saved_models
+
+echo ""
+read -p "enter the folder path after ../data/[..HERE..]: " name
+echo "
+
+copying
+  from: <ssh>/projects/thesis-code4/data/$name
+  to: <local>/Desktop/projects/thesis_stuff/Thesis-code/data/$name
+
+"
+while true; do
+    read -p "Confirm? (Y/N) " yn
+    case $yn in
+        [Yy]* ) scp -r yarcoh@132.72.49.187:projects/thesis-code4/data/$name ~/Desktop/projects/thesis_stuff/Thesis-code/data/$name; break;;
+        [Nn]* ) echo "np, canceling..";exit;;
+        * ) echo "Please answer Y or N.";;
+    esac
+done
