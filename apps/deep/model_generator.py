4c2939d3aeec40f9dbdb080658b064370c21e8f5
yarden <yarden9292@gmail.com>
2022-09-08 12:17:31 +0300

    fully document train config

--- apps/deep/model_generator.py
+++ apps/deep/model_generator.py
@@ -15,14 +15,14 @@ from src.deep.models import SingleMuModel3Layers
 
 @dataclass
 class TrainConfig:
-    run_name: str = field(default="test_model_10epochs")
-    epochs: int = field(default=10) # num of epochs
-    lr: float = field(default=1e-3)
-    batch_size: int = field(default=128)
-    train_val_ratio: float = field(default=0.8)
-    input_data_path: str = field(default='./data/datasets/qam1024_150x5/150_samples_mu=0.001')
-    output_model_path: str = field(default='./data/test_models')
-    device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+    run_name: str = field(default="test_model_10epochs") # name of the run in wandb
+    epochs: int = field(default=10)  # num of epochs
+    lr: float = field(default=1e-3) # learning rate
+    batch_size: int = field(default=128) # batch size
+    train_val_ratio: float = field(default=0.8) # train vs val ratio
+    input_data_path: str = field(default='./data/datasets/qam1024_150x5/150_samples_mu=0.001') # path to data
+    output_model_path: str = field(default='./data/test_models') # path to save model
+    device: str = 'cuda' if torch.cuda.is_available() else 'cpu' # device to use
 
 
 def main(config: TrainConfig):
