14ba04389889846a417cc497434185ed69934eb6
yarden <yarden9292@gmail.com>
2022-09-11 13:03:04 +0300

    improved how to.sh

--- scripts/how_to_execute_via_terminal.sh
+++ scripts/how_to_execute_via_terminal.sh
@@ -1,9 +1,11 @@
 echo """
+check existing stuffs..
+    echo $PYTHONPATH
+    conda env list
+
 run the following commands (dont copy):
 
     export PYTHONPATH=\"\$PYTHONPATH:\$PWD\"
-    conda env list
     conda activate thesis-code
-    python ./path/to/your_script.py
-
+    python ./apps/deep/multi_model_generator.py --config_path "./config/multi_model_generator/linux_test.yml"
 """
