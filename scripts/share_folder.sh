f4f69f259cbe3f6d0b328406779496993e3f962b
yarden <yarden9292@gmail.com>
2022-09-18 12:26:15 +0300

    improved ssh folder script

--- scripts/share_folder.sh
+++ scripts/share_folder.sh
@@ -1,14 +1,14 @@
-FOLDER_NAME="stas_config"
-PATH="./Desktop/$FOLDER_NAME"
-SSH_PATH="/home/yarcoh/projects/thesis-code4/config"
-if [ -d PATH ]; then
-    echo "Folder $PATH already exists,.. unmounting and deleting"
-    umount $PATH
-    rm -r $PATH
+#! /bin/bash
+mypath="${HOME}/Desktop/stas_folder"
+SSH_PATH="/home/yarcoh/projects/thesis-code4"
+if [ -d $mypath ]; then
+    echo "Folder ${mypath} already exists,.. unmounting and deleting"
+    umount $mypath
+    rm -r $mypath
     echo "unmounted and removed! now recreating.."
 else
-    echo "creating shared folder under $PATH"
+    echo "creating shared folder under $mypath"
 fi
-mkdir $PATH
-sshfs -o allow_other,default_permissions yarcoh@132.72.49.187:$SSH_PATH $PATH
-echo "all set! now you can access the folder $PATH"
+mkdir $mypath
+sshfs -o allow_other,default_permissions yarcoh@132.72.49.187:$SSH_PATH $mypath
+echo "all set! now you can access the folder $mypath"
