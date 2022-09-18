7b48da6cedcba99c678e4ab0ce6803461cff4427
yarden <yarden9292@gmail.com>
2022-09-18 18:33:32 +0300

    improved data analyzer

--- apps/deep/data_analyzer_app.py
+++ apps/deep/data_analyzer_app.py
@@ -8,19 +8,31 @@ from src.deep.data.data_analyzer import DataAnalyzer
 @dataclass
 class DataAnalyzerConfig:
     path: str = None
-    is_save_ber_vs_mu: bool = True
-    mu: float = None # mu to check if not None
-    i: int = None # data index to check if not None
+    mu: float = -1  # mu to check, if -1: takes the first folder
+    i: int = 0  # data index to check
+    is_full_ber: bool = True  # if True, calculate full ber
+    is_single_item: bool = False  # whether to plot single item (by i and mu vals)
     num_x_per_folder: int = 5
+    is_save_to_file: bool = True
+    is_upload_to_wandb: bool = True
 
 
 def main(config):
     path = config.path
     da = DataAnalyzer(path)
-    if config.mu is not None and config.i is not None:
-        da.save_sample_plots(config.mu, config.i)
-    if config.is_save_ber_vs_mu:
-        da.save_ber_vs_mu(config.num_x_per_folder)
+    if config.mu == -1:
+        config.mu = da.params['mu_start']
+
+    if config.is_single_item:
+        da.plot_single_sample(mu=config.mu, data_id=config.i, is_save=config.is_save_to_file)
+        if config.is_upload_to_wandb:
+            print('uploading single item to wandb is not fully supported yet...')  # TODO: complete this
+            da.wandb_log_single_sample(mu=config.mu, data_id=config.i)
+
+    if config.is_full_ber:
+        da.plot_full_ber_graph(n=config.num_x_per_folder, is_save=config.is_save_to_file)
+        if config.is_upload_to_wandb:
+            da.wandb_log_ber_vs_mu(n=config.num_x_per_folder)
 
 
 if __name__ == '__main__':
--- src/deep/data/data_analyzer.py
+++ src/deep/data/data_analyzer.py
@@ -13,74 +13,22 @@ analyzation_dir = '_analyzation'
 
 
 class DataAnalyzer():
-    def __init__(self, data_folder_path: str):
-        self.data_folder_path = data_folder_path.rstrip('/')
+    def __init__(self, data_folder_path: str, _tqdm=tqdm):
+        self.path = data_folder_path.rstrip('/')
         self.base_name = os.path.basename(data_folder_path)
         self.params = self.fetch_params()
         self.is_wandb_init = False
-
-    def _init_wandb(self):
-        data_type = self.params['conf']['data_type'] if 'data_type' in self.params['conf'] else 0
-        data_type = DataType(data_type).name
-        run_name = f'{self.base_name}__{data_type}'
-        wandb.init(project="data_analyze", entity="yarden92", name=run_name,
-                   tags=[f'{get_platform()}', f'ds={self.params["num_samples"]}'])
-        wandb.config = self.params
-        self.is_wandb_init = True
-
-    def wandb_log_ber_vs_mu(self, num_x_per_folder=5):
-        sub_name_filter = '*'
-        ber_vec, mu_vec = Metrics.gen_ber_mu_from_folders(self.data_folder_path, sub_name_filter, 0, tqdm,
-                                                          num_x_per_folder)
-        if not self.is_wandb_init:
-            self._init_wandb()
-        wandb.log({'BER vs mu': wandb.plot.line_series(
-            xs=mu_vec,
-            ys=[ber_vec],
-            keys=['ber'],
-            title='BER vs mu',
-            xname="mu")})
-
-    def save_sample_plots(self, mu: float, data_id: int):
-        # read folder and plot one of the data samples
-        num_samples = self.params['num_samples']
-        sub_name = f"{num_samples}_samples_mu={mu}"
-        dir = self.data_folder_path + '/' + sub_name
-
-        dataset = SingleMuDataSet(dir)
-
-        print(f'the folder {dir} contains {len(dataset)} samples')
-
-        x, y = dataset.get_numpy_xy(data_id)
-        out_path = f'{self.data_folder_path}/{analyzation_dir}/{sub_name}'
-        os.makedirs(out_path, exist_ok=True)
-
-        if self.params['conf']['data_type'] == 0:
-            zm = range(1700, 2300)
-            Visualizer.twin_zoom_plot('x', np.real(x), zm, output_name=out_path + '/x_real.png')
-            Visualizer.twin_zoom_plot('y', np.real(y), zm, output_name=out_path + '/y_real.png')
-        elif self.params['conf']['data_type'] == 1:
-            zm = range(0, 50)
-            Visualizer.twin_zoom_plot('x (real)', np.real(x), zm, function='stem', output_name=out_path + '/x_real.png')
-            Visualizer.twin_zoom_plot('y (real)', np.real(y), zm, function='stem', output_name=out_path + '/y_real.png')
-        else:
-            raise ValueError(f'unknown data type of {DataType(self.params["conf"]["data_type"]).name}')
-        print(f'images saved to {out_path}')
-
-    def save_ber_vs_mu(self, num_x_per_folder=5):
-        sub_name_filter = '*'
-        ber_vec, mu_vec = Metrics.gen_ber_mu_from_folders(self.data_folder_path, sub_name_filter, 0, tqdm,num_x_per_folder)
-        out_path = f'{self.data_folder_path}/{analyzation_dir}/ber_vs_mu.png'
-        Visualizer.plot_bers(mu_vec, [ber_vec],output_path=out_path)
-        print(f'ber vs mu saved to {out_path}')
+        self.ber_vec = None
+        self.mu_vec = None
+        self._tqdm = _tqdm
 
     def fetch_params(self):
         num_samples, num_mus = [int(x) for x in self.base_name.split('_')[-1].split('x')]
         mu_vec = []
         conf_list = []
-        for sub_name in os.listdir(self.data_folder_path):
+        for sub_name in os.listdir(self.path):
             if sub_name.startswith('_'): continue
-            sub_path = f"{self.data_folder_path}/{sub_name}"
+            sub_path = f"{self.path}/{sub_name}"
             conf = read_conf(sub_path)
             mu_vec.append(conf['normalization_factor'])
             conf_list.append(conf)
@@ -99,19 +47,113 @@ class DataAnalyzer():
         }
         return params
 
-    def set_params_manually(self, num_samples: int, mu_start: float, mu_end: float, num_mus: int):
-        self.params = {
-            'num_samples': num_samples,
-            # 'conf': conf,
-            'mu_start': mu_start,
-            'mu_end': mu_end,
-            'num_mus': num_mus
-        }
-        dataset = SingleMuDataSet(f"{self.data_folder_path}/{num_samples}_samples_mu={mu_start}")
-        self.params['conf'] = dataset.config
+    def _init_wandb(self):
+        if self.is_wandb_init: return
+        data_type = self.params['conf']['data_type'] if 'data_type' in self.params['conf'] else 0
+        data_type = DataType(data_type).name
+        run_name = f'{self.base_name}__{data_type}'
+        wandb.init(project="data_analyze", entity="yarden92", name=run_name,
+                   tags=[f'{get_platform()}', f'ds={self.params["num_samples"]}'])
+        wandb.config = self.params
+        self.is_wandb_init = True
+
+    def wandb_log_ber_vs_mu(self, n=5):
+        # n is the number of x's permutations to take from each folder
+        self._calc_full_ber(n)
+        self._init_wandb()
+        wandb.log({
+            'BER vs mu': wandb.plot.line_series(
+                xs=self.mu_vec,
+                ys=[self.ber_vec],
+                keys=['ber'],
+                title='BER vs mu',
+                xname="mu")
+        })
+
+    def wandb_log_single_sample(self, mu: float, data_id: int):
+        self._init_wandb()
+        sub_name = self._get_subfolder_name(mu)
+        x, y = self._get_xy(data_id, sub_name)
+        indices = np.arange(len(x))
+        for v, title in [(x, 'x (dirty)'), (y, 'y (clean)')]:
+            wandb.log({title: wandb.plot.line_series(
+                xs=indices,
+                ys=[v.real, v.imag],
+                keys=['real', 'imag'],
+                title=f'{title}, mu={mu}, i={data_id}',
+                xname="index")})
+
+        print(f'uploaded to wandb mu={mu}, i={data_id}')
+
+
+    def plot_single_sample(self, mu: float, data_id: int, is_save=True):
+        # read folder and plot one of the data samples
+        sub_name = self._get_subfolder_name(mu)
+        x, y = self._get_xy(data_id, sub_name)
+        out_path = f'{self.path}/{analyzation_dir}/{sub_name}'
+        os.makedirs(out_path, exist_ok=True)
+
+        is_spectrum = self.params['conf']['data_type'] == 0
+
+        zm = range(1700, 2300) if is_spectrum else range(0, 50)
+        func = 'plot' if is_spectrum else 'stem'
+
+        for v, name in [[x, 'x'], [y, 'y']]:
+            Visualizer.twin_zoom_plot(
+                title=name,
+                full_y=np.real(v),
+                zoom_indices=zm,
+                function=func,
+                output_name=f'{out_path}/{name}_real.png' if is_save else None
+            )
+
+        x_power = np.mean(np.abs(x) ** 2)
+        print(f'x_power={x_power}')
+
+        print(f'images saved to {out_path}')
+
+    def _get_xy(self, data_id, sub_name):
+        dir = self.path + '/' + sub_name
+        dataset = SingleMuDataSet(dir)
+        print(f'the folder {dir} contains {len(dataset)} samples')
+        x, y = dataset.get_numpy_xy(data_id)
+        return x, y
+
+    def plot_full_ber_graph(self, n=5, is_save=True):
+        # n is the number of x's permutations to take from each folder
+        self._calc_full_ber(n)
+        out_path = f'{self.path}/{analyzation_dir}/ber_vs_mu.png'
+
+        Visualizer.plot_bers(
+            us=self.mu_vec,
+            bers_vecs=[self.ber_vec],
+            output_path=out_path if is_save else None
+        )
+
+        print(f'ber vs mu saved to {out_path}')
+
+    def calc_ber_for_subfolder(self, mu, n=5):
+        # n is the number of x's permutations to take from each folder
+        sub_name = self._get_subfolder_name(mu)
+        dir = self.path + '/' + sub_name
+        dataset = SingleMuDataSet(dir)
+
+        ber_vec, num_errors = Metrics.calc_ber_from_dataset(dataset, False, self._tqdm, n)
+
+        print(f'the avg ber of mu={mu} (with {n} permutations) is {np.mean(ber_vec)}')
+
+    def _calc_full_ber(self, n):
+        sub_name_filter = '*'
+        if self.ber_vec is None or self.mu_vec is None:
+            self.ber_vec, self.mu_vec = Metrics.gen_ber_mu_from_folders(self.path, sub_name_filter, 0, self._tqdm, n)
+
+    def _get_subfolder_name(self, mu):
+        num_samples = self.params['num_samples']
+        sub_name = f"{num_samples}_samples_mu={mu:.3f}"
+        return sub_name
 
 
 if __name__ == '__main__':
     da = DataAnalyzer('./data/datasets/iq/qam1024_160x20')
-    da.save_sample_plots(0.001,0)
-    da.save_ber_vs_mu()
+    da.plot_single_sample(0.001, 0)
+    da.plot_full_ber_graph()
