4b3adc85742448fc0cb7559ec56b3893349e96c3
yarden <yarden9292@gmail.com>
2022-10-01 17:04:53 +0300

    cleanup

--- src/deep/metrics.py
+++ src/deep/metrics.py
@@ -31,13 +31,17 @@ class Metrics:
         return ber_i, num_errors_i
 
     @staticmethod
-    def calc_ber_for_folder(all_x_read, all_y_read, conf_read, verbose=True):
+    def calc_ber_for_folder(all_x_read, all_y_read, conf_read, verbose=True, tqdm=None):
         num_errors = 0
         ber_vec = []
         cs = ChannelSimulator.from_dict(conf_read)
         datatype = conf_read['data_type'] if 'data_type' in conf_read else DataType.spectrum
 
-        for i, (x, y) in enumerate(zip(all_x_read, all_y_read)):
+        rng = enumerate(zip(all_x_read, all_y_read))
+        if tqdm is not None:
+            rng = tqdm(rng,total=len(all_x_read), leave=False)
+
+        for i, (x, y) in rng:
             ber_i, num_errors_i = Metrics.calc_ber_for_single_vec(x, y, cs, datatype=datatype)
             ber_vec.append(ber_i)
             num_errors += num_errors_i
@@ -46,12 +50,12 @@ class Metrics:
         return ber_vec, num_errors
 
     @staticmethod
-    def calc_ber_from_dataset(dataset: OpticDataset, verbose=True, tqdm=None, num_x_per_folder=None):
+    def calc_ber_from_dataset(dataset: OpticDataset, verbose=True, _tqdm=None, num_x_per_folder=None):
         num_errors = 0
         ber_vec = []
         cs = ChannelSimulator.from_dict(dataset.config)
         N = min(num_x_per_folder or len(dataset), len(dataset))
-        rng = tqdm(range(N)) if tqdm else range(N)
+        rng = _tqdm(range(N)) if _tqdm else range(N)
         datatype = dataset.config['data_type'] if 'data_type' in dataset.config else DataType.spectrum
         for i in rng:
             x, y = dataset[i]
@@ -83,7 +87,7 @@ class Metrics:
         return ber_vec, num_errors
 
     @staticmethod
-    def gen_ber_mu_from_folders(root_dir, sub_name_filter, verbose_level=0, _tqdm=tqdm, num_x_per_folder=None):
+    def gen_ber_mu_from_folders(root_dir, sub_name_filter, verbose_level=0, _tqdm=tqdm, num_x_per_folder=None, is_matrixed_ber=False):
         # num_x_per_folder - number of samples to use from each folder to determine ber (averaging across), None=all
 
         # walk through folder [data] and search for folders that named as [10_samples_****]
@@ -94,7 +98,8 @@ class Metrics:
             all_x_read, all_y_read, conf_read = FilesReadWrite.read_folder(dirpath, verbose_level >= 1)
             all_x_read, all_y_read = trim_data(all_x_read, all_y_read, num_x_per_folder)
             sub_ber_vec, num_errors = Metrics.calc_ber_for_folder(all_x_read, all_y_read, conf_read, verbose_level >= 2)
-            ber = np.sum(sub_ber_vec)/len(sub_ber_vec)
+
+            ber = sub_ber_vec if is_matrixed_ber else np.mean(sub_ber_vec)
 
             mu_vec.append(mu)
             ber_vec.append(ber)
