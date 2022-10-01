0c8664db6a55105e9e42d9e87499a207ca439d78
yarden <yarden9292@gmail.com>
2022-10-01 21:02:34 +0300

    paper1 model v2

--- apps/deep/model_generator.py
+++ apps/deep/model_generator.py
@@ -7,51 +7,61 @@ import pyrallis
 from torch import nn
 from tqdm import tqdm
 
+from apps.deep.model_analyzer import ModelAnalyzer
 from src.deep import data_loaders
 from src.deep.data_loaders import SingleMuDataSet
 from src.deep.ml_ops import Trainer
 from src.deep import models
+from src.deep.standalone_methods import get_platform
 
 
 @dataclass
 class TrainConfig:
-    run_name: str = field(default="test_model_10epochs") # name of the run in wandb
-    epochs: int = field(default=10)  # num of epochs
-    lr: float = field(default=1e-3) # learning rate
-    batch_size: int = field(default=1) # batch size
-    train_val_ratio: float = field(default=0.8) # train vs val ratio
-    input_data_path: str = field(default='./data/datasets/qam1024_150x5/150_samples_mu=0.001') # path to data
-    output_model_path: str = field(default='./data/test_models') # path to save model
+    lr: float = 1e-3 # learning rate
+    epochs: int = 10  # num of epochs
+    batch_size: int = 1 # batch size
+    train_val_ratio: float = 0.8 # train vs val ratio
+    input_data_path: str = './data/datasets/qam1024_150x5/150_samples_mu=0.001' # path to data
+    output_model_path: str = './data/test_models' # path to save model
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu' # device to use
+    wandb_project: str = 'thesis_model_scan_test'
+    run_name: str = "test_model_10epochs" # name of the run in wandb
 
 
 def main(config: TrainConfig):
     # config
     print(f"Running {config.run_name}")
     print(json.dumps(config.__dict__, indent=4))
-    wandb.init(project="Thesis", entity="yarden92", name=config.run_name)
+    train_dataset, val_dataset = data_loaders.get_train_val_datasets(config.input_data_path, SingleMuDataSet,
+                                                                     train_val_ratio=config.train_val_ratio)
+
+    wandb.init(project=config.wandb_project, entity="yarden92", name=config.run_name,
+               tags=[f'mu={train_dataset.mu}', f'{get_platform()}', config.model_name + f'_real',
+                     f'ds={len(train_dataset)}']               )
     wandb.config = {
         "learning_rate": config.lr,
         "epochs": config.epochs,
         "batch_size": config.batch_size
     }
     l_metric = nn.MSELoss()  # or L1Loss
-    model = models.SingleMuModel3Layers()
-    # model_real = models.PaperNNforNFTmodel()
-    # model_imag = models.PaperNNforNFTmodel()
-    train_dataset, val_dataset = data_loaders.get_train_val_datasets(config.input_data_path, SingleMuDataSet,
-                                                                     train_val_ratio=config.train_val_ratio)
+    model = models.PaperNNforNFT_v2()
 
     optim = torch.optim.Adam(model.parameters(), lr=config.lr)
     trainer = Trainer(train_dataset=train_dataset, val_dataset=val_dataset, batch_size=config.batch_size,
                       model=model, device=config.device,
                       l_metric=l_metric, optim=optim,
                       params=config.__dict__)
-    trainer.model.print_architecture(train_dataset[0])
+    # trainer.model.print_architecture(train_dataset[0])
     trainer.train(num_epochs=config.epochs, _tqdm=tqdm)
     trainer.save3(config.output_model_path)
 
     print('finished training')
+
+    ma = ModelAnalyzer(trainer)
+    ma.upload_single_item_plots_to_wandb(i=0)
+    ma.upload_bers_to_wandb()
+
+
 
 
 if __name__ == '__main__':
--- src/deep/models.py
+++ src/deep/models.py
@@ -153,11 +153,44 @@ class PaperNNforNFTwrapper(nn.Module):
         self.imag_model = imag_model
 
     def forward(self, x: Tensor):
-        x_real = x[:,0]
-        x_imag = x[:,1]
+        x_real = x[:, 0]
+        x_imag = x[:, 1]
         pred_real = self.real_model(x_real)
         pred_imag = self.imag_model(x_imag)
         return torch.stack([pred_real, pred_imag], dim=1)
+
+
+class PaperNNforNFT_v2(nn.Module):
+    def __init__(self, input_size=8192, k=10):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self._1_conv = nn.Conv1d(in_channels=1, out_channels=10, kernel_size=k, stride=1, dilation=2)
+        self._1_tanh = nn.Tanh()
+        self._2_conv = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=k, stride=1, dilation=2)
+        self._2_tanh = nn.Tanh()
+        self._3_conv = nn.Conv1d(in_channels=10, out_channels=4, kernel_size=k, stride=1, dilation=2)
+        self._3_relu = nn.ReLU()
+        self._4_fc = nn.Linear(in_features=4*(input_size - 3*(2*(k - 1))), out_features=input_size)
+        # self._4_relu = nn.ReLU()
+
+    def forward(self, x_in: Tensor):
+        x_real = x_in[:, 0]
+        x_imag = x_in[:, 1]
+
+        for x in [x_real, x_imag]:
+            x = x.unsqueeze(1).unsqueeze(1).T
+            x = self._1_conv(x)
+            x = self._1_tanh(x)
+            x = self._2_conv(x)
+            x = self._2_tanh(x)
+            x = self._3_conv(x)
+            x = self._3_relu(x)
+            x = x.flatten()
+            x = self._4_fc(x)
+            # x = self._4_relu(x)
+
+        y = torch.stack([x_real, x_imag], dim=1)
+        return y
 
 
 class SingleMuModel3Layers(nn.Module):
