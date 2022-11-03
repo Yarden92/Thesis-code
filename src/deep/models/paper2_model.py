b6db4f4b9f726fcb93c3c93fd3ad908628c2f4f8
yarden <yarden9292@gmail.com>
2022-11-03 17:24:58 +0200

    apperantely paper2 is also dual-model type, fixed and tested

--- src/deep/models/paper2_model.py
+++ src/deep/models/paper2_model.py
@@ -4,7 +4,7 @@ from torch import nn, Tensor
 class Paper2Model(nn.Module):
     def __init__(self, input_size: int = 8192, k=10, s=1, d=2):
         super().__init__()
-        self._1_conv = nn.Conv1d(in_channels=2, out_channels=8, kernel_size=k, stride=s, dilation=d)
+        self._1_conv = nn.Conv1d(in_channels=1, out_channels=8, kernel_size=k, stride=s, dilation=d)
         self._1_relu = nn.ReLU()
         self._2_conv = nn.Conv1d(in_channels=8, out_channels=16, kernel_size=k, stride=s, dilation=d)
         self._2_relu = nn.ReLU()
@@ -16,11 +16,12 @@ class Paper2Model(nn.Module):
         self._5_relu = nn.ReLU()
         # self._6_conv = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=k, stride=s, dilation=d)
         # self._6_relu = nn.ReLU()
-        self._7_fc = nn.Linear(in_features=4*(input_size - 5*(2*(k - 1))), out_features=4*input_size)
+        self._7_fc = nn.Linear(in_features=4*(input_size - 5*(2*(k - 1))), out_features=2*input_size)
         self._7_relu = nn.ReLU()
-        self._8_fc = nn.Linear(in_features=4*input_size, out_features=2*input_size)
+        self._8_fc = nn.Linear(in_features=2*input_size, out_features=1*input_size)
 
     def forward(self, x: Tensor):
+        # input should be 1x2x8192, outputs the same
         x = self._1_conv(x)
         x = self._1_relu(x)
         x = self._2_conv(x)
@@ -33,7 +34,7 @@ class Paper2Model(nn.Module):
         x = self._5_relu(x)
         # x = self._6_conv(x)
         # x = self._6_relu(x)
-        x = x.flatten()
+        x = x.reshape([1,1,-1])
         x = self._7_fc(x)
         x = self._7_relu(x)
         x = self._8_fc(x)
