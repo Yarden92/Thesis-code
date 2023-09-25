f8beb1589762d6bbe251973dfdea51586d9f47a3
yarden-stas-pc <yarcoh@post.bgu.ac.il>
2023-09-25 16:54:58 +0300

    analyze big ds (b1)

--- apps/deep/data_generator_app.py
+++ apps/deep/data_generator_app.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         config = pyrallis.parse(DataConfig)
     else:
-        config_path = './config/data_generation/bad_area.yml'
+        config_path = './config/data_generation/quick_c.yml'
         config = pyrallis.parse(DataConfig, config_path=config_path)
     # main(config)
     DataGenerator(config).run()
